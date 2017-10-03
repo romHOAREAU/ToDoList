7ff83ad28da699608e2896393fae4216674d1f8b
rhoareau <romhoareaupro@gmail.com>
2017-10-03 15:24:34 +0200

    ajout commentaire pour montrer les lignes que j'avais fait

--- routes/task.js
+++ routes/task.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var taskControllers = require('../controllers/taskControllers');
+var taskControllers = require('../controllers/taskControllers'); //fait par Romain
 
 /* GET home page. */
+//fait par Romain (ligne 8-10)
 router.get('/list', taskControllers.task_list);
 router.get('/detail', taskControllers.task_detail);
 router.get('/post', taskControllers.task_ajout_post);
